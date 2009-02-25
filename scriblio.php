c57671e9355514cfc82c0b5e0dafcffc5c14068b
Casey Bisson <casey.bisson@gmail.com>
2009-02-25 21:33:12 +0000

    minor update to version number
    git-svn-id: http://plugins.svn.wordpress.org/scriblio/trunk@97701 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scriblio.php
+++ scriblio.php
@@ -3,7 +3,7 @@
 Plugin Name: Scriblio
 Plugin URI: http://about.scriblio.net/
 Description: Leveraging WordPress as a library OPAC.
-Version: 2.7 b05
+Version: 2.7-r1
 Author: Casey Bisson
 Author URI: http://maisonbisson.com/blog/
 */

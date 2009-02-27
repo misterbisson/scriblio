a36fba3b995151c8b3b6976eb2080977e950439b
Casey Bisson <casey.bisson@gmail.com>
2009-02-27 22:48:17 +0000

    fixes to default css
    git-svn-id: http://plugins.svn.wordpress.org/scriblio/trunk@98327 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scriblio.php
+++ scriblio.php
@@ -3,7 +3,7 @@
 Plugin Name: Scriblio
 Plugin URI: http://about.scriblio.net/
 Description: Leveraging WordPress as a library OPAC.
-Version: 2.7.1 b00
+Version: 2.7-r2
 Author: Casey Bisson
 Author URI: http://maisonbisson.com/blog/
 */

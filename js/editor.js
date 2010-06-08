7fa99006225b4eadbf1955473a0ff56d9c1a7b7e
Casey Bisson <casey.bisson@gmail.com>
2010-06-08 13:29:12 +0000

    moved some marcish-specific js pieces to that plugin, added a css fix
    git-svn-id: http://plugins.svn.wordpress.org/scriblio/trunk@249984 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/editor.js
+++ js/editor.js
@@ -15,32 +15,6 @@ jQuery.fn.scrib_renumber = function() {
 	});
 };
 
-
-function scrib_meditor() {
-	jQuery("div.wrap h2:first").text("Record Details");
-
-	jQuery("#bsuite_machinetags").hide();
-	jQuery("#categorydiv").hide();
-	jQuery("#trackbacksdiv").hide();
-	jQuery("#tagsdiv").hide();
-	jQuery("#postcustom").hide();
-	jQuery("#passworddiv").hide();
-	jQuery("#postexcerpt").hide();
-	jQuery("#postdivrich").hide();
-
-	jQuery("#titlediv #title").css({ marginLeft:"5px", width:"98%" });
-	jQuery("#titlediv").addClass("postbox");
-	jQuery("#titlediv #titlewrap").before('<h3 class="hndle"><label for="title">Primary Title</label></h3>');
-
-	jQuery("#scrib_meditor_div h3").text("Details");
-	jQuery("#scrib_meditor_div").removeClass('closed');
-	jQuery("#scrib_meditor_div").insertAfter("#titlediv");
-
-	jQuery("#bsuite_post_icon h3").text("Image");
-	jQuery("#bsuite_post_icon").removeClass('closed');
-	jQuery("#bsuite_post_icon").insertAfter('#titlediv');
-}
-
 jQuery(document).ready(function(){
 	// make the list sortable
 	// http://docs.jquery.com/UI/Sortables

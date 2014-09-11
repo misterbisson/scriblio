5ffc804840b6301e81e72f5a036108ed550ab2ec
Matthew Batchelder <borkweb@gmail.com>
2014-09-11 10:54:22 -0400

    Wrap selected facets in a div

--- plugin/widgets.php
+++ plugin/widgets.php
@@ -254,7 +254,7 @@ class Scrib_Searcheditor_Widget extends WP_Widget
 			echo '<div class="textwidget scrib_search_edit context-top">' . $context_top . '</div>';
 		}// end if
 
-		echo '<ul class="facets">'. scriblio()->facets()->editsearch() .'</ul>';
+		echo '<div class="selected-facets"><ul class="facets">'. scriblio()->facets()->editsearch() .'</ul></div>';
 
 		if ( ! empty( $context_bottom ) )
 		{
